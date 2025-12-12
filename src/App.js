47e08bcc4b675adebaa170788cc466f35e7f82b3
abisheksg <heyitsmeabishek@gmail.com>
2025-12-12 10:38:25 +0530

    first time in react with ungal abishek

---
+++ src/App.js
@@ -0,0 +1,78 @@
+/*function App(){
+  return(
+    <div>
+      <center>
+    <h1>Welcome To React JS</h1>
+     <h2>Welcome buddy !!</h2>
+      <p>lorefenfne kfneinveavnkaen kanfeinfienvkaefnvk ae fkaenfiaenneaklf kenfienienloenmvl mgoegoemoemr kengoengroengrmlo kngofenrglenrglewnmrl ngoenblomblsml goenmvoemvolemvlo elgqelgkmqelrmgolqerngloe oeveongoqengoern olognqeorngoqerngo oaefjqeromgoqengro ojfqeorjgoqrejmg olfjerogoqerjgo ojrfoqerjgoer oanfoqejroqer oagjoqeerjgoqerja oajaeojgeoajro ogjeqoajgoqer 
+      
+      </p>
+      </center>
+        </div>
+  )
+}*/
+// class compnents
+
+    import React from "react"
+    class App extends React.Component
+    {
+      render(){
+        return(
+          <div>
+            <center>
+            <h1> RAJINI MURUGAN THUNAI</h1>
+            <h2> Welcome Abishek !!</h2>
+            Qui reprehenderit nulla culpa incididunt elit pariatur magna commodo est consequat. Eu excepteur incididunt officia commodo reprehenderit ut. Qui cupidatat qui est adipisicing Lorem anim adipisicing culpa aute fugiat incididunt magna. Ea amet ut do tempor aliquip excepteur eiusmod sint. <mark>Aliquip dolore proident officia consectetur pariatur magna reprehenderit veniam Lorem</mark> reprehenderit eiusmod duis sit. Culpa esse ullamco aliquip non mollit elit reprehenderit excepteur proident.Ex mollit laborum anim incididunt laboris anim velit ad. Consequat tempor minim proident exercitation duis nostrud. Nisi quis enim ut nisi id irure deserunt eu occaecat ea id deserunt. Irure velit dolor in incididunt eu consectetur excepteur nulla. Est ut proident veniam minim qui nostrud. Quis dolor laborum pariatur fugiat ipsum est est laboris ut consequat commodo.
+
+Pariatur eiusmod adipisicing irure dolore deserunt nostrud aliquip adipisicing. Ea et duis nostrud anim sunt qui. Enim magna <mark>laboris mollit exercitation irure. Consectetur exercitation fugiat velit do nisi labore </mark> sit enim id laborum fugiat aliquip do.
+
+Ex quis proident veniam excepteur occaecat id sit enim esse anim labore fugiat. Magna ut ex consequat dolor consequat non. Ea ad amet mollit pariatur deserunt Lorem.
+
+Tempor Lorem laboris et Lorem consectetur ipsum Lorem amet aute consequat. Non adipisicing voluptate sunt do voluptate ad cillum. Anim nulla ad occaecat elit pariatur non nulla aliquip dolor occaecat laboris in.
+
+Consectetur occaecat magna laborum reprehenderit in minim sunt nisi dolor exercitation nostrud labore est officia. Sint ut dolore dolore exercitation excepteur laboris excepteur tempor occaecat proident. Adipisicing do et elit consectetur aliquip officia ea. Non do laboris in qui ullamco eu magna enim aliquip sunt aute exercitation mollit nostrud. Aliqua cupidatat excepteur aliquip velit est culpa sunt minim. Sunt exercitation exercitation minim nulla culpa voluptate sit ad ut occaecat nisi commodo deserunt veniam.
+
+Et do dolore aute sunt voluptate duis officia laborum excepteur sint irure. Eu nisi anim sit incididunt eiusmod fugiat qui ea fugiat dolor consectetur dolore. Excepteur culpa minim nisi ullamco adipisicing aliqua magna ea qui mollit. Anim deserunt aliquip eiusmod laborum sint adipisicing officia cupidatat quis fugiat tempor eu consectetur.
+
+Sint eu officia ullamco ipsum dolor cupidatat deserunt exercitation dolore proident enim magna irure ullamco. Tempor enim eiusmod consectetur culpa laboris cillum est occaecat magna. Consectetur et ut tempor dolore elit pariatur excepteur excepteur cillum ipsum. Sunt laborum eu ex cillum irure laboris ex ex laboris consectetur commodo irure. Eu quis pariatur occaecat ullamco nostrud adipisicing cillum id sint ipsum. Commodo nulla adipisicing occaecat non ullamco qui in sint dolor consectetur ex adipisicing. Eiusmod occaecat officia ipsum enim amet ipsum in qui eu proident ex officia voluptate.
+
+In tempor veniam aute qui voluptate. Duis dolore do esse officia irure magna reprehenderit esse cillum velit enim laborum. Commodo veniam pariatur ex ea est magna incididunt non.Occaecat fugiat consequat commodo laboris ullamco minim id aliqua occaecat non commodo reprehenderit in occaecat. Duis laboris voluptate voluptate eiusmod amet sint commodo tempor anim consequat dolore officia officia aliquip. Id aute ipsum anim ipsum non dolore dolor eu et anim ea quis magna irure. Ut consectetur duis cillum ad. Ad labore consequat et est magna.
+            Sunt ea id laboris velit veniam est magna sint do fugiat non reprehenderit. Aliqua ad nisi nulla nisi laboris nisi mollit cupidatat excepteur mollit. Minim culpa dolor non officia voluptate consectetur mollit sit aute occaecat officia consectetur. Voluptate laborum consequat nisi consectetur.Est non tempor sint elit sunt nulla occaecat consequat non ut. Veniam quis proident nisi sit quis laboris nulla reprehenderit ullamco. Et occaecat voluptate ad aute consequat enim quis do veniam occaecat voluptate esse labore ad. Adipisicing nisi ex nostrud cupidatat excepteur cillum Lorem aute consequat duis officia. Est magna laborum est elit proident.
+            
+            </center>
+          </div>
+        )
+      }
+    }
+
+export default App
+
+
+
+
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
